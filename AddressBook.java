bf8f0d9f2b685c8be7cbc9fccfbdf46c1d6a364a
Karthik <jeefkarthik036@gmail.com>
2022-04-20 19:58:42 +0530

    Second-added UC6_ToAdd_Multiple_AddressBook

---
+++ AddressBook.java
@@ -0,0 +1,16 @@
+package com.AddressBook_D22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+}
